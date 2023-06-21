dd35bbcfd5e5bffc4cc762eae8009590f118598c
Daria Kasko <dasha.volynets123@gmail.com>
2023-06-21 15:51:40 +0300

    DZ12_1 (lyambda) + DZ12_2 (reference to method)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+
         Book book1 = new Book("Pride and prejudice", "Jane Austen");
         Book book2 = new Book("Matilda", "Roald Dahl");
         Magazine magazine1 = new Magazine("Vogue", "Fashion");
         Magazine magazine2 = new Magazine("Drive", "Automobiles");
+
 
         Printable[] arrayOfCorrespondense = new Printable[4];
         arrayOfCorrespondense[0] = book1;
@@ -21,5 +28,17 @@ public class Main {
 
         System.out.println("\nWork of printBooks method using instanceof: ");
         Book.printBooks(arrayOfCorrespondense);
+
+        //DZ_12_1
+        Printable print = () -> System.out.println();
+
+        //DZ_12_2
+        List<Object> listOfCorrespondense = new ArrayList<>(4);
+        listOfCorrespondense.add(book1);
+        listOfCorrespondense.add(book2);
+        listOfCorrespondense.add(magazine1);
+        listOfCorrespondense.add(magazine2);
+
+        listOfCorrespondense.forEach(System.out::println);
     }
 }
